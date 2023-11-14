6b5327a72aee2e2f23bc771287e3bbb2e651caa1
MaxmudovIsmoil <mismoil0422@gmail.com>
2023-11-14 11:15:53 +0500

    first commit

---
+++ database/migrations/2023_11_14_105535_create_debtor_details_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('debtor_details', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('debtor_id');
+            $table->string('money')->nullable();
+            $table->enum('status', [1, -1]);
+            $table->timestamps();
+            $table->foreign('debtor_id')->references('id')->on('debtors')->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('debtor_detailes');
+    }
+};
---
+++ database/migrations/2023_11_14_110239_create_debtor_money_differences_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('debtor_money_differences', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('debtor_id');
+            $table->string('money')->nullable();
+            $table->enum('status', [1, -1]); // 1 + haqdor, -1 - qarzdor
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('restrict');
+            $table->foreign('debtor_id')->references('id')->on('debtors')->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('debtor_money_differences');
+    }
+};
